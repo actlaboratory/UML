608eaf34c4b67801d0eb1304cb2359ecc7e23347
Yukio Nozawa <personal@nyanchangames.com>
2022-07-30 15:08:27 +0900

    Fix menu + Add switching strategy list

--- addon/globalPlugins/UML/constants.py
+++ addon/globalPlugins/UML/constants.py
@@ -1,5 +1,3 @@
-# coding: UTF-8
-
 import addonHandler
 import config
 import languageHandler
--- addon/globalPlugins/UML/settings.py
+++ addon/globalPlugins/UML/settings.py
@@ -11,22 +11,37 @@ class SettingsDialog(wx.Dialog):
     def __init__(self):
         wx.Dialog.__init__(
             self, None, -1, _("Universal Multilingual settings"), size=(800, 600))
+
         lang = _("Primary language")
-        label = wx.StaticText(self, wx.ID_ANY, label=lang, name=lang)
-        self.langList = wx.ListCtrl(self, wx.ID_ANY, name=lang)
-        self.langList.InsertItem(0, _("Japanese"))
-        self.langList.InsertItem(1, _("Non-Japanese"))
+        langLabel = wx.StaticText(self, wx.ID_ANY, label=lang, name=lang)
+        self.langList = wx.ListBox(self, wx.ID_ANY, name=lang)
+        self.langList.InsertItems([_("Japanese"), _("non-Japanese")], 0)
+
+        strategy = _("Switching strategy")
+        strategyLabel = wx.StaticText(
+            self, wx.ID_ANY, label=strategy, name=strategy)
+        self.strategyList = wx.ListBox(self, wx.ID_ANY, name=strategy)
+        self.strategyList.InsertItems([_("Word"), _("Sentence")], 0)
+
         ok = wx.Button(self, wx.ID_OK, _("OK"))
         ok.SetDefault()
         cancel = wx.Button(self, wx.ID_CANCEL, _("Cancel"))
-        isz = wx.BoxSizer(wx.VERTICAL)
-        isz.Add(label, 1, wx.EXPAND)
-        isz.Add(self.langList, 1, wx.EXPAND)
+
+        lsz = wx.BoxSizer(wx.HORIZONTAL)
+        lsz.Add(langLabel, 1, wx.EXPAND)
+        lsz.Add(self.langList, 1, wx.EXPAND)
+
+        ssz = wx.BoxSizer(wx.HORIZONTAL)
+        ssz.Add(strategyLabel, 1, wx.EXPAND)
+        ssz.Add(self.strategyList, 1, wx.EXPAND)
+
         bsz = wx.BoxSizer(wx.HORIZONTAL)
         bsz.Add(ok, 1, wx.EXPAND)
         bsz.Add(cancel, 1, wx.EXPAND)
+
         msz = wx.BoxSizer(wx.VERTICAL)
-        msz.Add(isz, 1, wx.EXPAND)
+        msz.Add(lsz, 1, wx.EXPAND)
+        msz.Add(ssz, 1, wx.EXPAND)
         msz.Add(bsz, 1, wx.EXPAND)
         self.SetSizer(msz)
 
